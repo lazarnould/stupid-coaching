7d1d01a86596721231b9ab599c7ff5831839754d
laszlo arnould <laszlo.arnould@gmail.com>
2016-03-21 14:54:31 +0100

    stupid coaching on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'coaching#ask'
+
+  get 'ask', to: 'coaching#ask'
+  get 'answer', to: 'coaching#answer'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
